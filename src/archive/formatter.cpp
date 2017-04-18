ba92722f1fa11cc186e4e03828ee547ec1661da2
Job Talle <jobtalle@hotmail.com>
2017-04-18 18:07:37 +0200

    Started changing png format

--- src/archive/formatter.cpp
+++ src/archive/formatter.cpp
@@ -1,15 +1,25 @@
 #include <archive/formatter.h>
 #include <lodepng/lodepng.h>
 #include <iostream>
+#include <stdint.h>
 
 std::vector<char> formatter::formatPng(std::vector<char> file)
 {
 	unsigned char *result;
-	unsigned int width, height;
+	uint32_t width, height;
 
 	lodepng_decode_memory(&result, &width, &height, (unsigned char*)file.data(), file.size(), LCT_RGBA, 8);
 	
-	return std::vector<char>(result, result + width * height * 4);
+	std::vector<char> pixels = std::vector<char>(result, result + width * height * 4);
+
+	pixels.insert(pixels.begin(), (unsigned char*)&height, (unsigned char*)&height + 4);
+	pixels.insert(pixels.begin(), (unsigned char*)&width, (unsigned char*)&width + 4);
+
+	for(int i = 0; i < 8; ++i)
+		std::cout << pixels.data()[i];
+	std::cout << std::endl;
+
+	return pixels;
 }
 
 std::vector<char> formatter::format(std::vector<char> file, const std::string extension)
