fb323d2b025bcfc316c07f12d1d6d255cf4849ee
jobtalle <jobtalle@hotmail.com>
2017-06-27 02:45:58 +0200

    JSON files are read

--- src/archive/formats/formatPng.cpp
+++ src/archive/formats/formatPng.cpp
@@ -2,11 +2,15 @@
 #include <lodepng/lodepng.h>
 #include <iostream>
 #include <stdint.h>
+#include <utils/stringUtils.h>
+#include <picojson/picojson.h>
+#include <fstream>
 
 std::vector<char> formatter::formatPng(std::vector<char> file, const std::string &name)
 {
 	unsigned char *result;
 	uint32_t width, height;
+	const std::string json = name.substr(0, name.length() - utils::getExtension(name).length()) + "json";
 
 	lodepng_decode_memory(&result, &width, &height, (unsigned char*)file.data(), file.size(), LCT_RGBA, 8);
 
@@ -15,6 +19,29 @@ std::vector<char> formatter::formatPng(std::vector<char> file, const std::string
 
 	memcpy(header.data(), &width, sizeof(uint32_t));
 	memcpy(header.data() + sizeof(uint32_t), &height, sizeof(uint32_t));
+
+	std::ifstream inFile(json);
+
+	if(inFile.is_open())
+	{
+		picojson::value config;
+		inFile >> config;
+
+		std::vector<float> frameLengths;
+
+		picojson::value::object &object = config.get<picojson::object>()["frames"].get<picojson::object>();
+
+		for(picojson::value::object::const_iterator i = object.begin(); i != object.end(); ++i)
+		{
+			picojson::value::object frame = i->second.get<picojson::value::object>();
+
+			frameLengths.push_back(1000.0f / float(frame["duration"].get<double>()));
+
+			std::cout << frame["duration"] << std::endl;
+		}
+
+		std::cout << frameLengths.size() << " frames read" << std::endl;
+	}
 
 	pixels.insert(pixels.begin(), header.begin(), header.end());
 
